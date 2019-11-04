1f0e102b0c5c6e7adc07c63524a209f63b64f1e6
Nikolai Vazquez <nikvzqz@gmail.com>
2019-11-04 14:37:16 -0500

    Add fields to config
    Added `send_logs` and `send_logs_rate` for sending logging information
    to Ocean's backend for analysis.

--- lib/src/cfg/mod.rs
+++ lib/src/cfg/mod.rs
@@ -2,6 +2,8 @@
 //!
 //! This is tied heavily to `InstallTarget`.
 
+use std::time::Duration;
+
 pub mod file;
 #[doc(inline)]
 pub use self::file::{CfgFile, CfgFileFmt};
@@ -9,5 +11,19 @@ pub use self::file::{CfgFile, CfgFileFmt};
 /// Represents the configuration of a local installation.
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Cfg {
-    // TODO: ???
+    /// Whether to send logs to ensure correct behavior.
+    pub send_logs: bool,
+    /// How often to send logs if `send_logs` is `true`. The default is 1 week.
+    pub send_logs_rate: Duration,
+}
+
+impl Default for Cfg {
+    fn default() -> Self {
+        const DAY_SECS: u64 = 86400;
+        const WEEK_SECS: u64 = DAY_SECS * 7;
+        Cfg {
+            send_logs: false,
+            send_logs_rate: Duration::from_secs(WEEK_SECS),
+        }
+    }
 }
