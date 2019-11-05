8c70663d45fcae59ce2a53e5263b355e76380b20
Nikolai Vazquez <nikvzqz@gmail.com>
2019-11-05 11:23:12 -0500

    Add missing file for `OsStrExt`

---
+++ lib/src/ext/ffi.rs
@@ -0,0 +1,24 @@
+use std::ffi::OsStr;
+
+/// Extended functionality for
+/// [`OsStr`](https://doc.rust-lang.org/std/ffi/struct.OsStr.html).
+pub trait OsStrExt {
+    /// Attempts to retrieve the underlying bytes of `self`.
+    fn try_as_bytes(&self) -> Option<&[u8]>;
+}
+
+impl OsStrExt for OsStr {
+    #[inline]
+    fn try_as_bytes(&self) -> Option<&[u8]> {
+        #[cfg(unix)] {
+            use std::os::unix::ffi::OsStrExt;
+            Some(self.as_bytes())
+        }
+
+        // To get the bytes on non-Unix platforms, `OsStr` needs to be converted
+        // to a `str` first.
+        #[cfg(not(unix))] {
+            s.to_str().map(|s| s.as_bytes())
+        }
+    }
+}
