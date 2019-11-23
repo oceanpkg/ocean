31f43218388b4724ef66fd4fdfb984b8519d7681
Nikolai Vazquez <nikvzqz@gmail.com>
2019-11-23 10:23:32 -0500

    Move serde stuff for `License` to its own module

--- lib/src/drop/license/mod.rs
+++ lib/src/drop/license/mod.rs
@@ -4,10 +4,8 @@ use std::{
     fmt,
     borrow::Cow,
 };
-use serde::{
-    ser::{Serialize, Serializer},
-    de::{self, Deserialize, Deserializer, Visitor},
-};
+
+mod serde;
 
 #[doc(inline)]
 pub use license::{SpdxLicense, License as KnownLicense, Expr};
@@ -63,55 +61,6 @@ impl fmt::Display for License<'_> {
             License::Known(known) => known.fmt(f),
             License::Unknown(unknown) => unknown.fmt(f),
         }
-    }
-}
-
-struct LicenseVisitor;
-
-impl<'de> Visitor<'de> for LicenseVisitor {
-    type Value = License<'de>;
-
-    #[inline]
-    fn expecting(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.write_str("a license string")
-    }
-
-    #[inline]
-    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-        where E: de::Error,
-    {
-        Ok(License::owned(v))
-    }
-
-    #[inline]
-    fn visit_borrowed_str<E>(self, v: &'de str) -> Result<Self::Value, E>
-        where E: de::Error,
-    {
-        Ok(License::from(v))
-    }
-
-    #[inline]
-    fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
-        where E: de::Error,
-    {
-        Ok(License::owned(v))
-    }
-}
-
-impl<'de: 'a, 'a> Deserialize<'de> for License<'a> {
-    #[inline]
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>
-    {
-        deserializer.deserialize_str(LicenseVisitor)
-    }
-}
-
-impl Serialize for License<'_> {
-    #[inline]
-    fn serialize<S: Serializer>(&self, s: S) -> Result<S::Ok, S::Error> {
-        s.serialize_str(self.id())
     }
 }
 
