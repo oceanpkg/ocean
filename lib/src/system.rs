ae8ab145198b82254196ee2e0bc14aa5ac5d37f4
Nikolai Vazquez <nikvzqz@gmail.com>
2019-12-07 14:40:00 -0500

    Refactor code around opening resources
    Removed the `system::open_cmd` function in favor of:
    - An `OPEN_TOOL` constant to expose the specific tool for the current
      platform.
    - An `open` function for opening multiple resources simultaneously.
    
    Added Windows support for opening resources via the `start` command.

--- lib/src/system.rs
+++ lib/src/system.rs
@@ -2,26 +2,73 @@
 
 use std::{
     ffi::OsStr,
-    process::Command,
+    io,
+    process::{Command, Child, ExitStatus},
 };
 
-/// Returns a [`Command`] for opening a `resource` in the user's preferred
-/// application.
-///
-/// [`Command`]: https://doc.rust-lang.org/std/process/struct.Command.html
-pub fn open_cmd<R: AsRef<OsStr>>(resource: R) -> Command {
+/// The tool for opening resources in the users's preferred application.
+pub const OPEN_TOOL: &str = {
     cfg_if! {
         if #[cfg(target_os = "macos")] {
             let open = "/usr/bin/open";
         } else if #[cfg(target_os = "linux")] {
             let open = "xdg-open";
+        } else if #[cfg(target_os = "windows")] {
+            let open = "start";
         } else {
             compile_error!(
                 "No known way of opening a resource on the current platform."
             );
         }
     };
-    let mut cmd = Command::new(open);
-    cmd.arg("--").arg(resource);
-    cmd
+    open
+};
+
+/// Opens `resources` in the user's preferred application.
+pub fn open<R>(resources: &[R]) -> io::Result<()>
+    where R: AsRef<OsStr>
+{
+    fn status_to_result(status: ExitStatus) -> io::Result<()> {
+        if status.success() {
+            Ok(())
+        } else {
+            Err(io::Error::new(
+                io::ErrorKind::Other,
+                format!("Opening finished with status {}", status),
+            ))
+        }
+    }
+
+    // Do nothing if no resources were provided.
+    if resources.is_empty() {
+        return Ok(());
+    }
+
+    if cfg!(target_os = "macos") {
+        // The tool supports providing multiple resources at once.
+        let mut cmd = Command::new(OPEN_TOOL);
+        cmd.args(resources);
+        status_to_result(cmd.status()?)
+    } else {
+        // The tool needs to be spawned multiple times in order to open multiple
+        // resources.
+
+        // Spawn all processes before attempting to get their exit statuses.
+        // This ensures we don't fail early if a single resource couldn't be
+        // opened for any reason.
+        let children: Vec::<io::Result<Child>> = resources
+            .iter()
+            .map(|res| {
+                let mut cmd = Command::new(OPEN_TOOL);
+                cmd.arg(res);
+                cmd.spawn()
+            })
+            .collect();
+
+        for child in children {
+            status_to_result(child?.wait()?)?;
+        }
+
+        Ok(())
+    }
 }
