a878aa10c95b62ad8da2ef6040845c4633c399e6
Nikolai Vazquez <nikvzqz@gmail.com>
2019-12-08 15:36:17 -0500

    Move `api::v1::DEFAULT_URL` below imports

--- lib/src/api/v1.rs
+++ lib/src/api/v1.rs
@@ -1,10 +1,10 @@
 //! Version 1 of Ocean's web API.
 
-/// The default URL to which API calls are made: `https://api.oceanpkg.org/v1/`.
-pub const DEFAULT_URL: &str = "https://api.oceanpkg.org/v1/";
-
 #[cfg(feature = "reqwest")]
 use std::io;
+
+/// The default URL to which API calls are made: `https://api.oceanpkg.org/v1/`.
+pub const DEFAULT_URL: &str = "https://api.oceanpkg.org/v1/";
 
 #[cfg(feature = "reqwest")]
 fn convert_parse_error(error: url::ParseError) -> io::Error {
