ca6a5ea4b7c5e83175a2360f2e2562740ab2f208
Nikolai Vazquez <nikvzqz@gmail.com>
2019-12-08 15:34:17 -0500

    Add `api::v1::DEFAULT_URL`

--- lib/src/api/v1.rs
+++ lib/src/api/v1.rs
@@ -1,5 +1,8 @@
 //! Version 1 of Ocean's web API.
 
+/// The default URL to which API calls are made: `https://api.oceanpkg.org/v1/`.
+pub const DEFAULT_URL: &str = "https://api.oceanpkg.org/v1/";
+
 #[cfg(feature = "reqwest")]
 use std::io;
 
