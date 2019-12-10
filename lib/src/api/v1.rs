f59dc30adacb4ba9139d17d712da4afbbdce7ee4
Nikolai Vazquez <nikvzqz@gmail.com>
2019-12-10 15:59:00 -0500

    Create dedicated `api::v1::LoginError` type

--- lib/src/api/v1.rs
+++ lib/src/api/v1.rs
@@ -1,15 +1,7 @@
 //! Version 1 of Ocean's web API.
 
-#[cfg(feature = "reqwest")]
-use std::io;
-
 /// The default URL to which API calls are made: `https://api.oceanpkg.org/v1/`.
 pub const DEFAULT_URL: &str = "https://api.oceanpkg.org/v1/";
-
-#[cfg(feature = "reqwest")]
-fn convert_parse_error(error: url::ParseError) -> io::Error {
-    io::Error::new(io::ErrorKind::InvalidData, error)
-}
 
 /// Requests an API login token from [`url`].
 ///
@@ -20,8 +12,8 @@ fn convert_parse_error(error: url::ParseError) -> io::Error {
 pub fn request_login_token(
     username: &str,
     password: &str,
-) -> io::Result<String> {
-    let url = super::url().map_err(convert_parse_error)?;
+) -> Result<String, LoginError> {
+    let url = super::url()?;
     request_login_token_at(&url, username, password)
 }
 
@@ -34,8 +26,8 @@ pub fn request_login_token_at(
     api_url: &url::Url,
     username: &str,
     password: &str,
-) -> io::Result<String> {
-    let url = api_url.join("/v1/login").map_err(convert_parse_error)?;
+) -> Result<String, LoginError> {
+    let url = api_url.join("/v1/login")?;
     request_login_token_at_specific(url.as_str(), username, password)
 }
 
@@ -45,7 +37,7 @@ pub fn request_login_token_at_specific<U: reqwest::IntoUrl>(
     url: U,
     username: &str,
     password: &str,
-) -> io::Result<String> {
+) -> Result<String, LoginError> {
     #[derive(Serialize)]
     struct LoginCredentials<'a> {
         username: &'a str,
@@ -57,19 +49,14 @@ pub fn request_login_token_at_specific<U: reqwest::IntoUrl>(
     fn request_token(
             builder: reqwest::RequestBuilder,
             credentials: &LoginCredentials,
-    ) -> io::Result<String> {
+    ) -> Result<String, LoginError> {
         let response = builder
             .json(credentials)
-            .send()
-            .map_err(|error| {
-                io::Error::new(io::ErrorKind::Other, error)
-            })?;
+            .send()?;
 
         let status = response.status();
         if !status.is_success() {
-            let error_kind = io::ErrorKind::Other;
-            let error = format!("received response \"{}\"", status);
-            return Err(io::Error::new(error_kind, error));
+            return Err(LoginError::from(status));
         }
 
         for cookie in response.cookies() {
@@ -78,9 +65,7 @@ pub fn request_login_token_at_specific<U: reqwest::IntoUrl>(
             }
         }
 
-        let error_kind = io::ErrorKind::InvalidData;
-        let error = "the API login token was not received";
-        return Err(io::Error::new(error_kind, error));
+        Err(LoginError::MissingToken)
     }
 
     request_token(
@@ -88,3 +73,61 @@ pub fn request_login_token_at_specific<U: reqwest::IntoUrl>(
         &LoginCredentials { username, password },
     )
 }
+
+/// An error returned when attempting to log into Ocean's API.
+#[cfg(feature = "reqwest")]
+#[derive(Debug)]
+pub enum LoginError {
+    /// Failed to parse a `Url`.
+    ParseUrl(url::ParseError),
+    /// Failed to send the request via `reqwest`.
+    Request(reqwest::Error),
+    /// Received an error status code.
+    Status(http::StatusCode),
+    /// Failed to authenticate (401 status).
+    Unauthorized,
+    /// Received a successful response, but no token was provided.
+    MissingToken,
+}
+
+#[cfg(feature = "reqwest")]
+impl From<url::ParseError> for LoginError {
+    fn from(error: url::ParseError) -> Self {
+        Self::ParseUrl(error)
+    }
+}
+
+#[cfg(feature = "reqwest")]
+impl From<reqwest::Error> for LoginError {
+    fn from(error: reqwest::Error) -> Self {
+        Self::Request(error)
+    }
+}
+
+#[cfg(feature = "reqwest")]
+impl From<http::StatusCode> for LoginError {
+    fn from(error: http::StatusCode) -> Self {
+        if error == http::StatusCode::UNAUTHORIZED {
+            Self::Unauthorized
+        } else {
+            Self::Status(error)
+        }
+    }
+}
+
+#[cfg(feature = "reqwest")]
+impl std::fmt::Display for LoginError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        use self::LoginError::*;
+        match self {
+            ParseUrl(error) => error.fmt(f),
+            Request(error) => error.fmt(f),
+            Status(code) => write!(f, "received response \"{}\"", code),
+            Unauthorized => write!(f, "incorrect username or password"),
+            MissingToken => write!(f, "login token was not received"),
+        }
+    }
+}
+
+#[cfg(feature = "reqwest")]
+impl std::error::Error for LoginError {}
